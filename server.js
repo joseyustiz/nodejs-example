2584b8413abec042db34de279b987565d8bb431a
Jose Yustiz <jose.yustiz@gmail.com>
2020-07-19 18:48:38 -0400

    redirected all traffic from / to /v1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import registerMiddlewares from "./middlewares";
 
 async function main() {
     registerMiddlewares(server);
-
+    server.all("/", (req, res) => res.redirect("/v1")); //redirect all traffic from / to /v1
     server.use("/v1", rootRouter);
 
     server.listen();
